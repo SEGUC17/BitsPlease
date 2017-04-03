1692e309a9b9ed7fc2554c7691f6961ed064e98e
alyaaamr <alyaa.attia@student.guc.edu.eg>
2017-04-03 15:34:12 +0200

    search function 1st step

---
+++ search.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var db = require('app/db.js');
+
+function searchInDb(cb){
+	db.db().collection('products').find({},{'name': 'nutrition'}).toArray(function(err, product) {
+		if (err) cb(err, null);
+		cb(null, product);
+	});
+}
+exports.searchInDb = searchInDb; 
