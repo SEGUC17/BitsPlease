a9d351d494221ca4f69e451afcfa92e122a73ab8
farahmohamed <farah.el-sayed@student.guc.edu.eg>
2017-04-23 12:31:53 +0200

    login & register

--- public/services/mainSrv.js
+++ public/services/mainSrv.js
@@ -12,7 +12,6 @@ app.factory('mainSrv', function(){
 		getBusiness : function(){
 			return this.business;
 		},
-<<<<<<< HEAD
 		setProduct : function(product){
 			this.product = product;
 		},
@@ -25,10 +24,5 @@ app.factory('mainSrv', function(){
 		getAd : function(){
 			return this.ad;
 		}	
-=======
-		getProduct : function(){
-			return this.product;
-		}
->>>>>>> 496ad5e6674d370bc4546aa8a6577c31af3871d3
 	}
 })
